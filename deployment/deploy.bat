1e6ac353217f38d54ce654fefcbb80203c22aee1
hinst <hinstance@yandex.ru>
2024-05-01 20:12:51 +0200

    Adding caddy-service

--- deployment/deploy.bat
+++ deployment/deploy.bat
@@ -1,3 +1,5 @@
 kubectl apply -f ../web-ui/deployment.yaml
 kubectl apply -f ../web-ui/service.yaml
+kubectl apply -f caddy.yaml
+kubectl apply -f caddy-service.yaml
 kubectl rollout restart deployment smart-progress-or-web-ui
