33b0a5dfd33c4ac4a9c3065ca7f72cefcf21de14
hinst <hinstance@yandex.ru>
2024-05-01 20:52:49 +0200

    Put SmartProgress-or into a separated namespace

--- deployment/deploy.bat
+++ deployment/deploy.bat
@@ -1,5 +1,8 @@
-kubectl apply -f ../web-ui/deployment.yaml
-kubectl apply -f ../web-ui/service.yaml
 kubectl apply -f caddy.yaml
 kubectl apply -f caddy-service.yaml
-kubectl rollout restart deployment smart-progress-or-web-ui
+kubectl rollout restart deployment web-router
+
+kubectl apply -f smart-progress-or.yaml
+kubectl apply -f ../web-ui/deployment.yaml
+kubectl apply -f ../web-ui/service.yaml
+kubectl rollout restart deployment --namespace=smart-progress-or web-ui
