5d3dbf98a38041953c82aad7a4b8c35cc69d98cc
hinst <hinstance@yandex.ru>
2024-05-01 22:42:57 +0200

    Deploy downloader as cron job

--- deployment/deploy.bat
+++ deployment/deploy.bat
@@ -2,5 +2,6 @@ kubectl apply -f web-router.yaml
 kubectl rollout restart deployment web-router
 
 kubectl apply -f smart-progress-or.yaml
+kubectl apply -f ../downloader/deployment.yaml
 kubectl apply -f ../web-ui/deployment.yaml
 kubectl rollout restart deployment --namespace=smart-progress-or web-ui
