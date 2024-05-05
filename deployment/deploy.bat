9a232c9d1c544f0cc97a292487914105bcb73eab
hinst <hinstance@yandex.ru>
2024-05-05 18:06:30 +0200

    Updating deploy.bat

--- deployment/deploy.bat
+++ deployment/deploy.bat
@@ -1,9 +1,4 @@
-kubectl create configmap web-router-config --from-file=Caddyfile -o yaml --dry-run=client | kubectl apply -f - &&^
-kubectl create configmap web-router-favicon --from-file=favicon.ico -o yaml --dry-run=client | kubectl apply -f - &&^
-kubectl apply -f web-router.yaml &&^
+helmfile apply &&^
 kubectl rollout restart deployment web-router &&^
-kubectl apply -f smart-progress-or.yaml &&^
-kubectl apply -f ../downloader/deployment.yaml &&^
-kubectl apply -f ../web-ui/deployment.yaml &&^
 kubectl rollout restart deployment --namespace=smart-progress-or web-ui
 echo %ERRORLEVEL%
